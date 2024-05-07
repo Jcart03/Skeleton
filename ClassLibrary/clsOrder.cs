ad4942557fe6ff84411feda35161a32984190e15
P2715990 <p2715990@my365.dmu.ac.uk>
2024-05-07 10:23:50 +0100

    Added to the tstOrders Test Class

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public Int32 OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string StaffName { get; set; }
+        public string OrderNotes { get; set; }
+        public DateTime OrderDate { get; set; }
+        public TimeSpan OrderTime { get; set; }
+        public bool OrderShipped { get; set; }
+
     }
 }
--- Testing3/tstOrders.cs
+++ Testing3/tstOrders.cs
@@ -4,16 +4,81 @@ using System;
 
 namespace Testing3
 {
-	[TestClass]
-	public class tstOrders
-	{
-		[TestMethod]
-		public void InstanceOK()
-		{
-			//create an instance of the class we want to create
-			clsOrders AnOrder = new clsOrder();
-			//test to see that it exists
-			Assert.IsNotNull(AnOrder);
-		}
-	}
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //test to see that it exists
+            Assert.IsNotNull(AnOrder);
+        }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 TestData = 1;
+            AnOrder.OrderID = TestData;
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerNamePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "James";
+            AnOrder.CustomerName = TestData;
+            Assert.AreEqual(AnOrder.CustomerName, TestData);
+        }
+
+        [TestMethod]
+        public void StaffNamePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "John";
+            AnOrder.StaffName = TestData;
+            Assert.AreEqual(AnOrder.StaffName, TestData);
+        }
+
+        [TestMethod]
+        public void OrderNotesPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "Leave behind green bin";
+            AnOrder.OrderNotes = TestData;
+            Assert.AreEqual(AnOrder.OrderNotes, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.OrderDate = TestData;
+            Assert.AreEqual(AnOrder.OrderDate, TestData);
+        }
+
+        [TestMethod]
+        public void OrderTimePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            TimeSpan TestData = DateTime.Now.TimeOfDay;
+            AnOrder.OrderTime = TestData;
+            Assert.AreEqual(AnOrder.OrderTime, TestData);
+        }
+
+        [TestMethod]
+        public void OrderShippedPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.OrderShipped = TestData;
+            Assert.AreEqual(AnOrder.OrderShipped, TestData);
+        }
+
+
+    }
 }
