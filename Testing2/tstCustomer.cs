4dc0992fd5bf6f4192f070154035b83a7dfefa20
jamesC <jdc9181@gmail.com>
2024-05-10 01:26:57 +0100

    Added the Find method and implemented Tests

--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -23,5 +23,125 @@ namespace Testing2
         public void Address() { clsCustomer ACustomer = new clsCustomer(); string TestData = ""; ACustomer.Address = TestData; Assert.AreEqual(ACustomer.Address, TestData); }
         [TestMethod]
         public void Timestamp() { clsCustomer ACustomer = new clsCustomer(); DateTime TestData = DateTime.Now.Date; ACustomer.Timestamp = TestData; Assert.AreEqual(ACustomer.Timestamp, TestData); }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Int32 CustomerID = 21;
+            Found = ACustomer.Find(CustomerID);
+        }
+        [TestMethod]
+        public void TestCustomerIdFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Customer_Id != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerTimestampFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Timestamp != Convert.ToDateTime("23/12/2022"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerLoggedInFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Logged_In != false)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerNameFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Name != "John")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerEmailFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Email != "John@email.com")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerPasswordFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Password != "pass")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+        [TestMethod]
+        public void TestCustomerAddressFound()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 CustomerId = 1;
+            Found = ACustomer.Find(CustomerId);
+
+            if (ACustomer.Address != "Addr")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
     }
 }
