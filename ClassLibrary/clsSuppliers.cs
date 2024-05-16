9885a7b0306cf18592a9d07d868c29d0edcf418d
Nikhil-Lad11 <P2716657@my365.dmu.ac.uk>
2024-05-17 00:39:40 +0100

    All checked though - prac 7

--- ClassLibrary/clsSuppliers.cs
+++ ClassLibrary/clsSuppliers.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSuppliers
     {
+        public bool Active { get; set; }
+        public int SuppliersId { get; set; }
+        public string SuppliersName { get; set; }
+        public string SuppliersAddress { get; set; }
+        public string PaymentTerms { get; set; }
+        public DateTime ContractStart { get; set; }
+        public bool IsActive { get; set; }
+        public int ContactNumber { get; set; }
     }
 }
