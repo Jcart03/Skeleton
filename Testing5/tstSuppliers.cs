f049372bfe62cfe1cecd79274b24cf951e29dc98
Nikhil-Lad11 <P2716657@my365.dmu.ac.uk>
2024-05-16 13:22:09 +0100

    Added testing for Suppliers class

---
+++ Testing5/tstSuppliers.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstSuppliers
+    {
+        [TestMethod]
+        public void TestMethodOK()
+        {
+            //create an instance of thee class we want to create
+            clsSuppliers AnSuppliers = new clsSuppliers();
+            //Test to see that it exists
+            Assert.IsNotNull(AnSuppliers);
+        }
+    }
+}
