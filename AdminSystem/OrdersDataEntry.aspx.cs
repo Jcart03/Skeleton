e1f1bcb17dcf0139fa39e4bfa118475c006346a3
P2715990 <P2715990@my365.dmu.ac.uk>
2024-05-18 15:05:32 +0100

    Added validation to OrdersDataEntry page

--- AdminSystem/OrdersDataEntry.aspx.cs
+++ AdminSystem/OrdersDataEntry.aspx.cs
@@ -17,23 +17,49 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnOK_Click(object sender, EventArgs e)
     {
         clsOrder AnOrder = new clsOrder();
-        //capture OrderId
+
+        // capture OrderId
         AnOrder.OrderId = Convert.ToInt32(txtOrderId.Text);
-        //capture CustomerName
-        AnOrder.CustomerName = txtCustomerName.Text;
-        //capture StaffName
-        AnOrder.StaffName = txtStaffName.Text;
-        //capture OrderNotes
-        AnOrder.OrderNotes = txtOrderNotes.Text;
-        //capture OrderDate
-        AnOrder.OrderDate = Convert.ToDateTime(txtOrderDate.Text);
-        //capture OrderItem
-        AnOrder.OrderItem = txtOrderItem.Text;
-        //capture OrderQuantity
-        AnOrder.OrderQuantity = Convert.ToInt32(txtOrderQuantity.Text);
-        //capture OrderShipped
+        // capture CustomerName
+        string CustomerName = txtCustomerName.Text;
+        // capture StaffName
+        string StaffName = txtStaffName.Text;
+        // capture OrderNotes
+        string OrderNotes = txtOrderNotes.Text;
+        // capture OrderDate
+        string OrderDate = txtOrderDate.Text;
+        // capture OrderItem
+        string OrderItem = txtOrderItem.Text;
+        // capture OrderQuantity
+        string OrderQuantity = txtOrderQuantity.Text;
+        // capture OrderShipped
         AnOrder.OrderShipped = chkOrderShipped.Checked;
-        Session["AnOrder"] = AnOrder;
-        Response.Redirect("OrdersViewer.aspx");
+
+        // validate and catch any error message       
+        string Error = AnOrder.Valid(CustomerName, StaffName, OrderNotes, OrderDate, OrderItem, OrderQuantity);
+        if (Error == "")
+        {
+            // capture validated CustomerName
+            AnOrder.CustomerName = CustomerName;
+            // capture validated StaffName
+            AnOrder.StaffName = StaffName;
+            // capture validated OrderNotes
+            AnOrder.OrderNotes = OrderNotes;
+            // capture validated OrderDate
+            AnOrder.OrderDate = Convert.ToDateTime(OrderDate);
+            // capture validated OrderItem
+            AnOrder.OrderItem = OrderItem;
+            // capture validated OrderQuantity
+            AnOrder.OrderQuantity = Convert.ToInt32(OrderQuantity);
+
+            // navigate to view page
+            Session["AnOrder"] = AnOrder;
+            Response.Redirect("OrdersViewer.aspx");
+        }
+        else
+        {
+            // display error message
+            lblError.Text = Error;
+        }
     }
 }
