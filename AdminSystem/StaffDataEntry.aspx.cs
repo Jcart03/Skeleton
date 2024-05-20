3aded9cac4a67429e3ad41ee888cd0c31c4eee42
mishrtananji <p2716659@my365.dmu.ac.uk>
2024-05-20 12:24:53 +0100

    created the find method - middle layer & data layer

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -32,4 +32,35 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the staff class
+        clsStaff AnStaff = new clsStaff();
+        //create a variable to store the primary key
+        Int32 StaffId;
+        //create a variable to store the result of the find operation 
+        Boolean Found = false;
+        //get the primary key entered by the user
+        StaffId = Convert.ToInt32(txtStaffID.Text);
+        //Find the record
+        Found = AnStaff.Find(StaffId);
+        //if found
+        if (Found == true)
+        {
+
+            //display the vakues of the properties in the form
+            txtStaffName.Text = AnStaff.StaffName;
+            txtStaffAddress.Text = AnStaff.StaffAddress;
+            txtStaffEmail.Text = AnStaff.StaffEmail;
+            txtStartingDate.Text = AnStaff.StartingDate.ToString("yyyy-MM-dd");
+            txtStaffSalary.Text = AnStaff.StaffSalary.ToString("F2");
+            chkIsManager.Checked = AnStaff.IsManager;
+        }
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+
+    }
 }
--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,14 +4,150 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
+        //private data member for the staff id property
+        private Int32 mStaffId;
+        //staffid public property
+        public Int32 StaffId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffId;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffId = value;
+            }
+        }
         public bool Active { get; set; }
         public DateTime DateAdded { get; set; }
-        public int StaffId { get; set; }
-        public string StaffName { get; set; }
-        public string StaffAddress { get; set; }
-        public string StaffEmail { get; set; }
-        public DateTime StartingDate { get; set; }
-        public decimal StaffSalary { get; set; }
-        public bool IsManager { get; set; }
+        //private data member for the staff name property
+        private string mStaffName;
+        //staff name public property
+        public String StaffName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffName;
+            }
+            set
+            {
+                mStaffName = value;
+            }
+        }
+
+        //private data member for the staff name property
+        private string mStaffAddress;
+        //staff name public property
+        public string StaffAddress
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffAddress;
+            }
+            set
+            {
+                mStaffAddress = value;
+            }
+        }
+
+
+        //private data member for the staff name property
+        private string mStaffEmail;
+        //staff name public property
+        public String StaffEmail
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffEmail;
+            }
+            set
+            {
+                mStaffEmail = value;
+            }
+        }
+
+
+        //private data member for the staff name property
+        private DateTime mStartingDate;
+        //staff name public property
+        public DateTime StartingDate
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStartingDate;
+            }
+            set
+            {
+                mStartingDate = value;
+            }
+        }
+
+        //private data member for the staff name property
+        private Decimal mStaffSalary;
+        //staff name public property
+        public Decimal StaffSalary
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffSalary;
+            }
+            set
+            {
+                mStaffSalary = value;
+            }
+        }
+
+        //private data member for the staff name property
+        private Boolean mIsManager;
+        //staff name public property
+        public bool IsManager
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mIsManager;
+            }
+            set
+            {
+                mIsManager = value;
+            }
+        }
+
+        public bool Find(int staffId)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the staff id to search for
+            DB.AddParameter("@StaffId", StaffId);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_FilterByStaffId");
+            //if one record is found (there should be either one or zero)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mStaffId = Convert.ToInt32(DB.DataTable.Rows[0]["StaffId"]);
+                mStaffName = Convert.ToString(DB.DataTable.Rows[0]["StaffName"]);
+                mStaffAddress = Convert.ToString(DB.DataTable.Rows[0]["StaffAddress"]);
+                mStaffEmail = Convert.ToString(DB.DataTable.Rows[0]["StaffEmail"]);
+                mStartingDate = Convert.ToDateTime(DB.DataTable.Rows[0]["StartingDate"]);
+                mStaffSalary = Convert.ToDecimal(DB.DataTable.Rows[0]["StaffSalary"]);
+                mIsManager = Convert.ToBoolean(DB.DataTable.Rows[0]["IsManager"]);
+                //return that everything worked ok
+                return true;
+            }
+            //if no record was found 
+            else
+            {
+                //return false indicating there is a problem
+                return false;
+            }
+        }
     }
 }
