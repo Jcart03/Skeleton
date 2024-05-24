17f6085f1e805cec6a673565f8c51ad9c61581f0
mishrtananji <p2716659@my365.dmu.ac.uk>
2024-05-24 09:55:51 +0100

    created the collection class properties

---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+
+    //contructor for the class
+    public class clsStaffCollection
+    {
+        //create the items of test data
+        clsStaff TestItem = new clsStaff();
+        //set its properties 
+
+
+        //private data member for the list
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                //return the private data
+                return mStaffList;
+            }
+            set
+            {
+                //set the private data
+                mStaffList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mStaffList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsStaff ThisStaff { get; set; }
+    }
+}
