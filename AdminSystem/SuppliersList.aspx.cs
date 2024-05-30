9edb33d1af25edd683b93b2cb19895f245ca2448
Nikhil-Lad11 <168381028+Nikhil-Lad11@users.noreply.github.com>
2024-05-30 14:25:37 +0100

    Revert "labs 8 & 9 & 11, 12 , 13 ,14 ,15,16"

--- AdminSystem/SuppliersList.aspx.cs
+++ AdminSystem/SuppliersList.aspx.cs
@@ -1,5 +1,4 @@
-﻿using ClassLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,77 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class _1_List : System.Web.UI.Page
 {
-    int SupplierId;
     protected void Page_Load(object sender, EventArgs e)
-    {
-        SupplierId = Convert.ToInt32(Session["SupplierId"]);
-        if (IsPostBack == false)
-        {
-            
-                DisplaySuppliers();
-        }
-    }
-    void DisplaySuppliers()
-    {
-        clsSuppliersCollection Suppliers = new clsSuppliersCollection();
-        lstSuppliersList.DataSource = Suppliers.SuppliersList;
-        lstSuppliersList.DataValueField = "SupplierId";
-        lstSuppliersList.DataTextField = "Suppliername";
-        lstSuppliersList.DataBind();
-    }
-
-    protected void btnAdd_Click(object sender, EventArgs e)
-    {
-        Session["SupplierId"] = -1;
-        Response.Redirect("SuppliersDataEntry.aspx");
-
-    }
-
-    protected void btnEdit_Click(object sender, EventArgs e)
-    {
-        int SupplierId;
-        if (lstSuppliersList.SelectedIndex != -1)
-        {
-            SupplierId = Convert.ToInt32(lstSuppliersList.SelectedValue);
-            Session["SupplierId"] =SupplierId;
-            Response.Redirect("SuppliersDataEntry.apsx");
-
-        }
-        else
-        {
-            lblError.Text = "Select a record from the list to edit";
-        }
-    }
-
-    protected void btnDelete_Click(object sender, EventArgs e)
-    {
-        int SupplierId;
-        if (lstSuppliersList.SelectedIndex != -1)
-        {
-            SupplierId = Convert.ToInt32(lstSuppliersList.SelectedValue);
-            Session["SupplierId"] = SupplierId;
-            Response.Redirect("SuppliersConfirmDelete.aspx");
-
-        }
-        else
-        {
-            lblError.Text = "Select a record from the list to delete";
-        }
-    }
-
-    
-
-    protected void btnreturntomm_Click(object sender, EventArgs e)
-    {
-        Response.Redirect("TeamMainMenu.aspx");
-    }
-
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        Response.Redirect("SupplierStatistics.aspx");
-    }
-
-    protected void txtFilter_TextChanged(object sender, EventArgs e)
     {
 
     }
--- AdminSystem/SuppliersViewer.aspx.cs
+++ AdminSystem/SuppliersViewer.aspx.cs
@@ -1,5 +1,4 @@
-﻿using ClassLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,14 +9,6 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        clsSuppliers anSuppliers = new clsSuppliers();
-        anSuppliers = (clsSuppliers)Session["anSuppliers"];
-        Response.Write(anSuppliers.SuppliersId);
-        Response.Write(anSuppliers.Suppliername);
-        Response.Write(anSuppliers.Address);
-        Response.Write(anSuppliers.PaymentTerms);
-        Response.Write(anSuppliers.ContractStart);
-        Response.Write(anSuppliers.ContactNumber);
-        Response.Write(anSuppliers.IsActive);
+
     }
 }
